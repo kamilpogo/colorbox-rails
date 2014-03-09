a6c8f4f32a68c079c6626d4fbf76b2983ce99b16
kamilpogo <kamilp@gmail.com>
2014-03-09 23:02:23 +0100

    Update colorbox-links.js
    fixed turbolinks

--- app/assets/javascripts/colorbox-rails/colorbox-links.js
+++ app/assets/javascripts/colorbox-rails/colorbox-links.js
@@ -66,6 +66,23 @@
 
       if($colorboxBox)
           $colorboxBox.appendTo("body");
+          
+      $('[data-colorbox-static="true"]').colorbox({ 
+      height: function() { return $(this).data("colorbox-height") || false },
+      width: function() { return $(this).data("colorbox-width") || false },
+      maxWidth: function() { return $(this).data("colorbox-max-width") || false },
+      maxHeight: function() { return $(this).data("colorbox-max-height") || false },
+      iframe: function() { return $(this).data("colorbox-iframe") || false },
+      photo: function() { return $(this).data("colorbox-photo") || false },
+      innerHeight: function() { return $(this).data("colorbox-innerheight") || false },
+      innerWidth: function() { return $(this).data("colorbox-innerwidth") || false },
+      title: function() { return $(this).data("colorbox-title") || false },
+      className: function() { return $(this).data("colorbox-class-name") || false },
+      href: function() { return $(this).data("colorbox-href") || $(this).attr('href') },
+      html: function() { return $(this).data("colorbox-html") || false },
+      inline: function() { return $(this).data("colorbox-inline") || false },
+      opacity: 0.5
+    });
   });      
 
 }) (jQuery);
